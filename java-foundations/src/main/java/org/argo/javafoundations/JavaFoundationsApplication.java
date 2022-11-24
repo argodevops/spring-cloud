965fde7ab15ef466f9dd5e7db63991ec5d35455a
VoidMainStringArgs <111274137+VoidMainStringArgs@users.noreply.github.com>
2022-11-24 14:41:56 +0000

    feat: add spring init files

---
+++ java-foundations/src/main/java/org/argo/javafoundations/JavaFoundationsApplication.java
@@ -0,0 +1,13 @@
+package org.argo.javafoundations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFoundationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFoundationsApplication.class, args);
+	}
+
+}
